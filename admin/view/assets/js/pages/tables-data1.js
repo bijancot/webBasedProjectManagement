a7e54e72c4b6ea57f42757633cee8b10613f56c1
budosen <panjidia995@gmail.com>
2019-09-11 07:53:50 +0700

    add view for role management

---
+++ admin/view/assets/js/pages/tables-data1.js
@@ -0,0 +1,122 @@
+'use strict';
+//  Author: ThemeREX.com
+//  tables-data.html scripts
+//
+
+(function($) {
+
+    $(document).ready(function() {
+
+        "use strict";
+
+        // Init Theme Core
+        Core.init();
+
+        // Init Demo JS
+        Demo.init();
+
+        // Init DataTables
+        $('#datatable').dataTable({
+            "sDom": 't<"dt-panelfooter clearfix"ip>',
+            "oTableTools": {
+                "sSwfPath": "assets/js/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+            }
+        });
+
+        $('#datatable2').DataTable({
+            "aoColumnDefs": [{
+                'bSortable': false,
+                'aTargets': [-1]
+            }],
+            "oLanguage": {
+                "oPaginate": {
+                    "sPrevious": "",
+                    "sNext": ""
+                }
+            },
+            "iDisplayLength": 5,
+            "aLengthMenu": [
+                [5, 10, 25, 50, -1],
+                [5, 10, 25, 50, "All"]
+            ],
+            "sDom": '<"dt-panelmenu clearfix"lfr>t<"dt-panelfooter clearfix"ip>',
+            "oTableTools": {
+                "sSwfPath": "assets/js/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+            }
+        });
+
+        $('#datatable3').dataTable({
+			order: [[ 0, 'desc' ]],
+            "aoColumnDefs": [{
+                'bSortable': false,
+                'aTargets': [-1]
+            }],
+            "oLanguage": {
+                "oPaginate": {
+                    "sPrevious": "",
+                    "sNext": ""
+                }
+            },
+            "iDisplayLength": 5,
+            "aLengthMenu": [
+                [5, 10, 25, 50, -1],
+                [5, 10, 25, 50, "All"]
+            ],
+            "sDom": '<"dt-panelmenu clearfix"Tfr>t<"dt-panelfooter clearfix"ip>',
+            "oTableTools": {
+                "sSwfPath": "assets/js/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+            }
+        });
+
+        $('#datatable4').dataTable({
+            "aoColumnDefs": [{
+                'bSortable': false,
+                'aTargets': [-1]
+            }],
+            "oLanguage": {
+                "oPaginate": {
+                    "sPrevious": "",
+                    "sNext": ""
+                }
+            },
+            "iDisplayLength": 5,
+            "aLengthMenu": [
+                [5, 10, 25, 50, -1],
+                [5, 10, 25, 50, "All"]
+            ],
+            "sDom": 'T<"panel-menu dt-panelmenu"lfr><"clearfix">tip',
+
+            "oTableTools": {
+                "sSwfPath": "assets/js/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+            }
+        });
+
+        // Multi-Column Filtering
+        
+
+        // DataTable
+       
+
+        // Apply search
+      
+
+        // ABC FILTERING
+       
+
+      
+
+
+        // ROW Arrange
+       
+
+        // Order by group
+       
+
+        // ROWS - collapsible
+      
+
+        // Placeholder for filter bar
+        $('.dataTables_filter input').attr("placeholder", "Search Terms...");
+    });
+
+})(jQuery);
