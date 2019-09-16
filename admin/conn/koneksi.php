37369a4626a0c546222476cb12eea81d3e37f798
budosen <panjidia995@gmail.com>
2019-09-16 22:51:19 +0700

    add base_url variable

--- admin/conn/koneksi.php
+++ admin/conn/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-	$host="localhost"; //host laptop bisa pakai 127.0.0.1 atau localhost
+	$host="192.168.79.3"; //host laptop bisa pakai 127.0.0.1 atau localhost
 	$dbname="mmopilot"; // nama database
-	$user="root"; // nama user dalam my sql
-	$password=""; // password user, jika kosong beri string kosong
+	$user="budosen"; // nama user dalam my sql
+	$password="bijan2089"; // password user, jika kosong beri string kosong
 	try{
 		$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
 	  	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -17,5 +17,5 @@
 	catch(PDOException $STH){
 	  echo "<br>".$STH->getMessage();
 	}
-
+$base_url = "http://localhost:8983/";
 ?>
