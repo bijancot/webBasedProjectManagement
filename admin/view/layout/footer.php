de6447a830933573d4a551dec9bcc017ed390e79
budosen <panjidia995@gmail.com>
2019-09-24 09:59:32 +0700

    edit footer untuk resource js

--- admin/view/layout/footer.php
+++ admin/view/layout/footer.php
@@ -15,59 +15,34 @@
 <script src="view/assets/fonts/animatedsvgicons/js/svgicons-init.js"></script>
 
 <!-- Scroll -->
-<script src="view/assets/js/utility/malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.concat.min.js"></script>
+<!-- <script src="view/assets/js/utility/malihu-custom-scrollbar-plugin-master/jquery.mCustomScrollbar.concat.min.js"></script> -->
 
 <!-- Mixitup -->
 
-
 <!-- Summernote -->
-
-
 
 <!-- HighCharts Plugin -->
 <script src="view/assets/js/plugins/highcharts/highcharts.js"></script>
 
 <!-- Highlight JS -->
 
-
 <!-- Date/Month - Pickers -->
 
-
-
-
-
 <!-- Magnific Popup Plugin -->
-<script src="view/assets/js/plugins/magnific/jquery.magnific-popup.js"></script>
+<!-- <script src="view/assets/js/plugins/magnific/jquery.magnific-popup.js"></script> -->
 
 <!-- FullCalendar Plugin -->
 
-
-
 <!-- Plugins -->
-
-
-<script src="view/assets/js/plugins/c3charts/d3.min.js"></script>
-<script src="view/assets/js/plugins/c3charts/c3.min.js"></script>
-
-
-
-
-
+<!--<script src="view/assets/js/plugins/c3charts/c3.min.js"></script>-->
 <script src="view/assets/js/plugins/circles/circles.js"></script>
-
-
-
 
 <!-- Google Map API -->
 
-
-
-
-
 <!-- Jvectormap JS -->
 <script src="view/assets/js/plugins/jvectormap/jquery.jvectormap.min.js"></script>
-<script src="view/assets/js/plugins/jvectormap/assets/jquery-jvectormap-us-lcc-en.js"></script>
-<script src="view/assets/js/plugins/jvectormap/assets/jquery-jvectormap-world-mill-en.js"></script>
+<!-- <script src="view/assets/js/plugins/jvectormap/assets/jquery-jvectormap-us-lcc-en.js"></script> -->
+<!-- <script src="view/assets/js/plugins/jvectormap/assets/jquery-jvectormap-world-mill-en.js"></script> -->
 
 <!-- Datatables JS -->
 <script src="view/assets/js/plugins/datatables/media/js/jquery.dataTables.js"></script>
@@ -75,90 +50,51 @@
 <script src="view/assets/js/plugins/datatables/extensions/ColReorder/js/dataTables.colReorder.min.js"></script>
 <script src="view/assets/js/plugins/datatables/media/js/dataTables.bootstrap.js"></script>
 
-
-
 <!-- FooTable JS -->
-
-
 
 <!-- Validate JS -->
 
-
-
 <!-- BS Dual Listbox JS -->
-
 
 <!-- Bootstrap Maxlength JS -->
 
-
 <!-- Select2 JS -->
-
 
 <!-- Typeahead JS -->
 
-
 <!-- TagManager JS -->
-
 
 <!-- DateRange JS -->
 
-
 <!-- BS Colorpicker JS -->
-
 
 <!-- MaskedInput JS -->
 
-
 <!-- Slick Slider JS -->
-
 
 <!-- MarkDown JS -->
 
-
-
-
 <!-- X-edit CSS -->
-
-
-
-
-
 
 <!-- Dropzone JS -->
 
-
 <!-- Cropper JS -->
-
 
 <!-- Zoom JS -->
 
-
 <!-- Nestable JS -->
-
 
 <!-- PNotify JS -->
 
-
 <!-- Fancytree JSs -->
-
-
-
-
-
-
 
 <!-- Ladda JS -->
 
-
 <!-- NProgress JS -->
-
 
 <!-- Countdown JS -->
 
-
-
 <!-- CanvasBG JS -->
-
 
 <!-- Theme Scripts -->
 <script src="view/assets/js/utility/utility.js"></script>
@@ -167,52 +103,14 @@
 <script src="view/assets/js/demo/widgets_sidebar.js"></script>
 
 
+<script src="view/assets/js/pages/dashboardcopy.js"></script>
 
 
-<!-- js ini tidak dibutuhkan, ketika ditambahkan justru membuat error bagian sidebar
-
-<script src="view/assets/js/pages/dashboard1.js"></script>
-
--->
 <script src="view/assets/js/pages/tables-data1.js"></script>
+<script src="view/assets/js/pages/allcp_forms-elements.js"></script>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<script src="view/assets/js/demo/widgets.js"></script>
-
-
-
-
-<!-- /Scripts -->
+<!--fixed, tidak membuat error-->
+<!-- <script src="view/assets/js/demo/widgets.js"></script> -->
 
 </body>
 
