9df1434d1a51953e2257388995c73f78237f311b
lunalotussa <171111047@mhs.stiki.ac.id>
2019-10-18 22:57:14 +0700

    Add files via upload
    gambar error

--- admin/model/edit_item.php
+++ admin/model/edit_item.php
@@ -6,11 +6,13 @@ $idItem			= htmlentities($_GET['id']);
 $itemName		= htmlentities($_POST['itemName']);
 $itemDescription= htmlentities($_POST['itemDescription']);
 $itemLink		= htmlentities($_POST['itemLink']);
+$itemIcon   	= $_FILES['itemIcon']['name'];
 
-
+move_uploaded_file($file_tmp, 'gambar/'.$itemIcon);
 $query = $db->prepare("UPDATE `mmo_item` SET 
 		`itemName`= :itemName,
 		`itemDescription`= :itemDescription,
+		`itemIcon`= :itemIcon,
 		`itemLink`= :itemLink,
 		`lastUpdate`=NOW()
 		WHERE `idItem`=:idItem");
@@ -19,6 +21,7 @@ $query = $db->prepare("UPDATE `mmo_item` SET
  $query->bindParam(":itemName", $itemName);
  $query->bindParam(":itemDescription", $itemDescription);
  $query->bindParam(":itemLink", $itemLink);
+ $query->bindParam(":itemIcon", $itemIcon);
  $query->execute();
  header('location:?mmopilot=manageitem');
 
