ac383e8a86a55075483ad7a186d5f2d47f1bc52a
budosen <panjidia995@gmail.com>
2019-10-22 12:38:29 +0700

    menambahkan banyak file, beberapa progress hilang :(

--- admin/model/input_item.php
+++ admin/model/input_item.php
@@ -6,7 +6,7 @@ $idItem				= htmlentities($_POST['idItem']);
 $itemName			= htmlentities($_POST['itemName']);
 $itemDescription	= htmlentities($_POST['itemDescription']);
 $itemLink			= htmlentities($_POST['itemLink']);
-
+$itemIcon           =$_FILES['itemIcon']['name'];
 
 
 $query = $db->prepare("INSERT INTO `mmo_item`(`idItem`, `itemName`, `itemDescription`, `itemIcon`, `itemLink`, `createdDate`, `lastUpdate`, `createdBy`, `editedBy`) VALUES (:idItem, :itemName, :itemDescription, '', :itemLink, NOW(), NOW(),'','')");
--- admin/view/layout/item/additem.php
+++ admin/view/layout/item/additem.php
@@ -11,21 +11,60 @@
                 <div class="mw1000 center-block">
 
                     <!-- Spec Form -->
-                    <div>
+                    <div class="allcp-form">
                         <div class="panel">
+                        <div class="panel-heading">
+                                <div class="row nm">
+                                    <div class="col-md-4"> 
+                                    </div>
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-4">  
+                                    <div class="panel-title hidden-xs">
+                                    ID Role : 
+                                    <?php
+                                        $select = $db->prepare("SELECT count(idRole) FROM mmo_role");
+                                                    $select->execute();
+                                                    $tampil = $select->fetchAll();
+                                          foreach($tampil as $value){
+                                            $ha = $value[0]+1;
+                                            if($ha>=0 && $ha<=9){
+                                                echo "ITEM000".$ha;
+                                            }else if($ha>=9 && $ha<=99){
+                                                echo "ITEM00".$ha;
+                                            }else if($ha>=99 && $ha<=999){
+                                                echo "ITEM0".$ha;
+                                            }else if($ha>=999 && $ha<=9999){
+                                                echo "ITEM".$ha;
+                                            }
+                                        }
+                                    ?>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>  
                             
                             <div class="panel-body">
+                            
 
                             <form class="form-horizontal" action="?mmopilot=input_item" method="POST" enctype="multipart/form-data">
                                 
-                                <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">ID Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="idItem">
-                                        </div>
-                                    </div>
-                                </div>
+                                    <?php
+                                        $select = $db->prepare("SELECT count(idRole) FROM mmo_role");
+                                                  $select->execute();
+                                                  $tampil = $select->fetchAll();
+                                          foreach($tampil as $value){
+                                            $ha = $value[0]+1;
+                                            if($ha>=0 && $ha<=9){
+                                                echo "<input type =\"hidden\" value=\"ITEM000".$ha."\" name=\"idItem\">";
+                                            }else if($ha>=9 && $ha<=99){
+                                                echo "<input type =\"hidden\" value=\"ITEM00".$ha."\" name=\"idItem\">";
+                                            }else if($ha>=99 && $ha<=999){
+                                                echo "<input type =\"hidden\" value=\"ITEM0".$ha."\" name=\"idItem\">";
+                                            }else if($ha>=999 && $ha<=9999){
+                                                echo "<input type =\"hidden\" value=\"ITEM".$ha."\" name=\"idItem\">";
+                                            }
+                                        }
+                                    ?>
 
                                 <div class="form-group">
                                     <label class="col-lg-3 control-label" for="textArea3">Nama Item</label>
