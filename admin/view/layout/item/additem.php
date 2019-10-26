91df5d9d2946b4c683041399e07d12680b369414
Panji Faith Baskoro <panjidia995@gmail.com>
2019-10-26 23:56:32 +0700

    Menambahkan tampilan untuk menambah item

--- admin/view/layout/item/additem.php
+++ admin/view/layout/item/additem.php
@@ -1,10 +1,24 @@
+
    <!-- Main Wrapper -->
     <section id="content_wrapper">
+                  <!-- Topbar -->
+                  <header id="topbar" class="alt">
+            <div class="topbar-left">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-link">
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-current-item">Manage Item > Add New Item</li>
+                </ol>
+            </div>
+        </header>
+        <!-- /Topbar -->
 
+        <div class="greeting-field">
+            Add New Item
+        </div>
         <!-- Content -->
         <section id="content" class="table-layout animated fadeIn">
-
-
             <!-- Column Center -->
             <div class="chute chute-center">
 
@@ -12,61 +26,56 @@
 
                     <!-- Spec Form -->
                     <div class="allcp-form">
-                        <div class="panel">
-                        <div class="panel-heading">
-                                <div class="row nm">
-                                    <div class="col-md-4"> 
-                                    </div>
-                                    <div class="col-md-4"></div>
-                                    <div class="col-md-4">  
-                                    <div class="panel-title hidden-xs">
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>  
+                        <div class="panel">  
                             
                             <div class="panel-body">
                             
-
                             <form class="form-horizontal" action="?mmopilot=input_item" method="POST" enctype="multipart/form-data">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="section">
+                                                <h5>Nama Item</h5>
+                                                    <label class="field prepend-icon">
+                                                        <input type="text" name="itemName" id="firstname" class="gui-input"
+                                                            placeholder="Nama Item" title="masukkan nama item di sini">
+                                                        <span class="field-icon">
+                                                            <i class="fa fa-slack"></i>
+                                                        </span>
+                                                    </label><br/><br/>
+                                                    
+                                                    <h5>Deskripsi Item</h5>
+                                                    <label class="field prepend-icon">
+                                                    <textarea class="gui-textarea" id="comment" name="roleDesc" placeholder="masukkan deskripsi item di sini"></textarea>
+                                                    <span class="field-icon" title="masukkan deskripsi item di sini">
+                                                        <i class="fa fa-list"></i>
+                                                    </span>
+                                                    </label><br/><br/>
 
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Nama Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="itemName">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-1"></div>
+                                            <div class="col-md-5">
+                                                <div class="section">
+                                                <h5>Link Item</h5>
+                                                    <label class="field prepend-icon">
+                                                        <input type="text" name="itemLink" class="gui-input"
+                                                            placeholder="Nama Item" title="masukkan link docs/knowledge base item tersebut ()">
+                                                        <span class="field-icon">
+                                                            <i class="fa fa-link"></i>
+                                                        </span>
+                                                    </label><br/><br/>
+                                                    <h5>Item Icon</h5>
+                                                    <label class="field prepend-icon">
+                                                        <input type="file" name="itemIcon" class="gui-input"
+                                                            placeholder="Nama Item" title="Upload Icon untuk Item">
+                                                        <span class="field-icon">
+                                                            <i class="fa fa-upload"></i>
+                                                        </span>
+                                                    </label><br/><br/><br/><br/><br/>
+                                                    <button type="submit" class="btn btn-block btn-success"><strong>Simpan Data</strong></button>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Deskripsi Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="itemDescription">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Icon Item</label>
-                                     <div class="col-lg-8">
-                                         <div>
-                                             <input type="file" name="itemIcon">
-                                          </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Link Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="itemLink">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <input type="submit" class="btn btn-rounded btn-primary btn-block"  name="submit" value="Simpan">
                             </form>
                         </div>
                         </div>
