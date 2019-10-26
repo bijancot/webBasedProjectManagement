d1a00e5d92ff83b9dda2129f7b844735a1cb54b0
Panji Faith Baskoro <panjidia995@gmail.com>
2019-10-26 23:56:05 +0700

    Memperbaiki tampilan menambah order

--- admin/view/layout/order/addorder.php
+++ admin/view/layout/order/addorder.php
@@ -1,36 +1,38 @@
    <!-- Main Wrapper -->
     <section id="content_wrapper">
+    <header id="topbar" class="alt">
+            <div class="topbar-left">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-link">
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-current-item">Manage order > Add New Order</li>
+                </ol>
+            </div>
+        </header>
+        <!-- /Topbar -->
+
+        <div class="greeting-field">
+            Add New Order
+        </div>
 
         <!-- Content -->
         <section id="content" class="table-layout animated fadeIn">
-
-
             <!-- Column Center -->
             <div class="chute chute-center">
-
                 <div class="mw1000 center-block">
 
                     <!-- Spec Form -->
-                    <div>
+                    <div class="allcp-form">
                         <div class="panel">
-                            
                             <div class="panel-body">
 
                             <form class="form-horizontal" action="?mmopilot=input_order" method="POST" enctype="multipart/form-data">
-                                
-                                <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">ID Order</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="idOrder">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Nama User</label>
-                                    <div class="col-lg-8">
-                                        <div>
+                                <div class="row">
+                                    <div class="col-md-5">
+                                    <h5>Pilih User</h5>
+                                    <label class="field prepend-icon">
+                                        <label class="field select">
                                             <select class="form-control" name="idUser">
                                                 <option>-------- Pilih User --------</option>
                                                 <?php
@@ -40,67 +42,78 @@
                                                     foreach($tampil as $value){
                                                 ?>
                                                 <option value="<?php echo $value['idUser']?>"><?php echo $value['name']?></option>
-                                                <?php }?>
+                                            <?php }?>
                                             </select>
-                                        </div>
-                                    </div>
-                                </div>
+                                        <i class="arrow"></i>
+                                        </label>
+                                    </label><br/><br/>
 
-                                 <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">Mata Uang</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="currency">
-                                        </div>
-                                    </div>
-                                </div>
+                                    <h6>Username</h6>
+                                    <label class="field prepend-icon">
+                                        <input type="text" name="username" id="firstname" class="gui-input"
+                                        placeholder="Username" title="masukkan username di sini">
+                                            <span class="field-icon">
+                                                <i class="fa fa-dollar"></i>
+                                            </span>
+                                    </label><br/><br/>
 
-                                 <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">Nominal</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="price">
-                                        </div>
-                                    </div>
-                                </div>
+                                    <h6>Password</h6>
+                                    <label class="field prepend-icon">
+                                        <input type="text" name="password" id="firstname" class="gui-input"
+                                        placeholder="Password" title="masukkan password order">
+                                            <span class="field-icon">
+                                                <i class="fa fa-dollar"></i>
+                                            </span>
+                                    </label><br/><br/>
 
-                                 <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">Catatan Order</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="orderNote">
-                                        </div>
+                                    <label class="field prepend-icon">
+                                        <input type="text" name="konpassword" id="firstname" class="gui-input"
+                                        placeholder="Konfirmasi Password" title="Konfirmasi Password order">
+                                            <span class="field-icon">
+                                                <i class="fa fa-dollar"></i>
+                                            </span>
+                                    </label><br/><br/>
                                     </div>
-                                </div>
-
-                                 <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">Username</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="username">
-                                        </div>
+                                    <div class="col-md-1"></div>
+                                    <div class="col-md-6"> 
+                                    <h5>Mata Uang</h5>
+                                <div class="row section prn">
+                                    <div class="col-md-6">
+                                    <label class="option">
+                                        <input type="radio" name="currency" value="USD">
+                                        <span class="radio"></span>US Dollar ($)
+                                    </label>
                                     </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">Password</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="password" id="inputStandard" class="form-control" name="password">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                 <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">Konfirmasi Password</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="password" id="inputStandard" class="form-control" name="konpassword">
-                                        </div>
-                                    </div>
+                                    <div class="col-md-6">
+                                    <label class="option">
+                                        <input type="radio" name="currency" value="Rupiah">
+                                        <span class="radio"></span>Rupiah (IDR)
+                                    </label>
+                                    
+                                    </div><br/><br/><br/>
+                                    * pilih mata uang
                                 </div>
                                 
-                                <input type="submit" class="btn btn-rounded btn-primary btn-block"  name="submit" value="Simpan">
+                                <h5>Nominal</h5>
+                                    <label class="field prepend-icon">
+                                        <input type="text" name="price" id="firstname" class="gui-input"
+                                        placeholder="Nominal Order" title="masukkan biaya order">
+                                            <span class="field-icon">
+                                                <i class="fa fa-dollar"></i>
+                                            </span>
+                                </label><br/><br/>
+                                    
+                                <h5>Order Notes</h5>
+                                    <label class="field prepend-icon">
+                                        <textarea class="gui-textarea" id="comment" name="orderNote" placeholder="Tambahkan catatan order disini jika ada"></textarea>
+                                        <span class="field-icon">
+                                          <i class="fa fa-list"></i>
+                                        </span>
+                                    </label><br/><br/><br/>
+
+                                <button type="submit" class="btn btn-block btn-success" name="submit"><strong>Simpan Data</strong></button>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                         </div>
