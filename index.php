050b41ce7b039883b524230ef9eb15bff0b71140
lunalotussa <171111047@mhs.stiki.ac.id>
2019-10-30 19:32:16 +0700

    Add files via upload
    menambahkan index register,login,aksi login

--- index.php
+++ index.php
@@ -5,8 +5,19 @@ $mmolog = $_REQUEST["mmolog"];
 switch ($mmolog){
 
 default:
-	require 'login/register.php';
+	require 'login/login.php';
 break;
+
+//aksi login
+case 'add_login':
+ 	require 'login/add_login.php';
+break;
+
+//register
+case 'register':
+ 	require 'login/register.php';
+break;
+
 //aksi register
 case 'add_register':
  	require 'login/add_register.php';
