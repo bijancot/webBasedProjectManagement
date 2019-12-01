fe9a09dbb6bff390e9a267077d92b63a58225359
Panji Faith Baskoro <panjidia995@gmail.com>
2019-12-01 11:45:55 +0700

    menyesuaikan session

--- login/session.php
+++ login/session.php
@@ -2,13 +2,13 @@
 include("../admin/conn/koneksi.php");
 session_start();
 $user_check=$_SESSION['login_user'];
-$admin = $db->prepare('SELECT * FROM mmo_users WHERE username = :username');
+$admin = $db->prepare('SELECT * FROM mmo_users WHERE email = :username');
 $admin->execute(array(
                   ':username' => $user_check
                   ));
 $row = $admin->fetch(PDO::FETCH_ASSOC);
 
-$login_session=$row['username'];
+$login_session=$row['email'];
 $login_job=$row['job'];
 $login_role=$row['roleId'];
 
