92fdf3f346be085fbf012250c287b285558492d8
lunalotussa <171111047@mhs.stiki.ac.id>
2019-12-08 22:20:29 +0700

    Add files via upload
    membenarkan aksi login (email pass)

--- login/add_login.php
+++ login/add_login.php
@@ -4,20 +4,23 @@ if(isset($_POST['submit'])){
 session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-$admin = $db->prepare('SELECT * FROM mmo_users WHERE email = :username and passhash = :passcode');
+$admin = $db->prepare('SELECT * FROM mmo_users WHERE email = :email and passhash = :passcode');
 $admin->execute(array(
-                  ':username' => $_POST['username'],
+                  ':email' => $_POST['email'],
                   'passcode' => md5($_POST['password'])
                   ));
 $row = $admin->fetch(PDO::FETCH_ASSOC);
  
-if(empty($row['email'])){
-echo "<script type=\"text/javascript\">alert('Your Login Name or Password is invalid');document.location='?mmolog';</script>";
-
+if($row['passhash']==md5($_POST['password'])&&$row['email']==$_POST['email']){
+//echo "<script type=\"text/javascript\">alert('Your Login Name or Password is invalid');document.location='?mmolog';</script>";
+$_SESSION['login_user'] = $_POST['email'];
+header("location: admin/index.php");
 }
 else {
-$_SESSION['login_user'] = $_POST['username'];
-header("location: admin/index.php");
+echo "<script type=\"text/javascript\">alert('Your Login Name or Password is invalid');document.location='?mmolog';</script>";
+// $_SESSION['login_user'] = $_POST['username'];
+// header("location: admin/index.php");
+
 }
 }}
 ?>
