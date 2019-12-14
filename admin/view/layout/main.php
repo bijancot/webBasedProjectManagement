b4e9dc667745afa23c12298d909bdafff459a0de
Panji Faith Baskoro <panjidia995@gmail.com>
2019-12-14 21:05:36 +0700

    benerin dashboard

--- admin/view/layout/main.php
+++ admin/view/layout/main.php
@@ -47,35 +47,145 @@
 
                 <!-- Topbar -->
         <header id="topbar" class="alt">
-            <div class="topbar-left">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-link">
-                        <a href="index.html">Home</a>
-                    </li>
-                    <li class="breadcrumb-current-item">Dashboard</li>
-                </ol>
-            </div>
+          
         </header>
         <!-- /Topbar -->
 
-        <div class="greeting-field">
-            Welcome back, <span><?php echo $login_session; ?></span>!
-        </div>
+
 
         <!-- Content -->
         <section id="content" class="table-layout animated fadeIn">
-
             <!-- Column Center -->
             <div class="chute chute-center">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="panel panel-tile info-block">
+                        <div class="panel-body">
+                                <div class="info-block-top">
+                                    <i class="icon-check text-dark"></i>
+                                </div>
+                                <div class="info-block-middle text-center">
+                                    <h4>6.794</h4>
 
+                                </div>
+                                <div class="info-block-bottom text-center">
+                                    <h6>Order</h6>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="panel panel-tile info-block">
+                    <div class="panel-body">
+                                <div class="info-block-top">
+                                    <i class="icon-people text-dark"></i>
+                                </div>
+                                <div class="info-block-middle text-center">
+                                    <h4>6.794</h4>
+
+                                </div>
+                                <div class="info-block-bottom text-center">
+                                    <h6>Operator Aktif</h6>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="panel panel-tile info-block">
+                    <div class="panel-body">
+                                <div class="info-block-top">
+                                    <i class="icon-flag text-dark"></i>
+                                </div>
+                                <div class="info-block-middle text-center">
+                                    <h4>6.794</h4>
+
+                                </div>
+                                <div class="info-block-bottom text-center">
+                                    <h6>Job Selesai</h6>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="panel panel-tile info-block">
+                        <div class="panel-body">
+                                <div class="info-block-top">
+                                    <i class="icon-clock text-dark"></i>
+                                </div>
+                                <div class="info-block-middle text-center">
+                                    <h4>6.794</h4>
+
+                                </div>
+                                <div class="info-block-bottom text-center">
+                                    <h6>Job Dalam Pengerjaan</h6>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
                 <!-- AllCP Info -->
                 <!-- SubNav Chart -->
-                <div class="panel" id="pchart12">
-                    <div class="panel-heading">
-                        <span class="panel-title pn"> Sub Navigation Chart</span>
+                <div class="row">
+                    <div class="col-md-6">
+                            <div class="panel" id="pchart12">
+                            <div class="panel-heading">
+                                <span class="panel-title pn">Grafik Penerimaan</span>
+                            </div>
+                            <div class="panel-body bg-light dark">
+                                <div id="subnav-chart" style="height: 300px;"></div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="panel-body bg-light dark">
-                        <div id="subnav-chart" style="height: 350px; width: 100%;"></div>
+                    <div class="col-md-6">
+                    <div class="panel pn" id="spy6">
+                                <div class="panel-heading ">
+                                    <span class="panel-title">Job Prioritas tinggi</span>
+                                </div>
+                                <div class="panel-body br-t pt20 ph30 bp25">
+                                    <div>
+                                        <div class="table-responsive">
+                                            <table class="table table-striped table-style-3">
+                                                <thead class="">
+                                                <tr>
+                                                    <th class="">Project</th>
+                                                    <th class=" hidden-xs">Start Date</th>
+                                                    <th class="">Deadline</th>
+                                                    <th class="">Status</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr>
+                                                    <td class="wp25">#4551</td>
+                                                    <td class="hidden-xs">10.02.2016</td>
+                                                    <td>10.02.2016</td>
+                                                    <td><span class="label label-info">in progress</span></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="">#4532</td>
+                                                    <td class="hidden-xs">23.12.2015</td>
+                                                    <td>11.01.2016</td>
+                                                    <td><span class="label label-primary">Done</span></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="">#4536</td>
+                                                    <td class="hidden-xs">16.05.2015</td>
+                                                    <td>18.06.2015</td>
+                                                    <td><span class="label label-danger">Failed</span></td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="">#4552</td>
+                                                    <td class="hidden-xs">10.02.2016</td>
+                                                    <td>10.02.2016</td>
+                                                    <td><span class="label label-info">In Progress</span></td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
 
