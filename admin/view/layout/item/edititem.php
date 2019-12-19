1cc7119df6927d738b69e657b238eca2a75d2b22
lunalotussa <171111047@mhs.stiki.ac.id>
2019-12-19 21:59:22 +0700

    Add files via upload

--- admin/view/layout/item/edititem.php
+++ admin/view/layout/item/edititem.php
@@ -1,76 +1,92 @@
+
    <!-- Main Wrapper -->
     <section id="content_wrapper">
+                  <!-- Topbar -->
+                  <header id="topbar" class="alt">
+            <div class="topbar-left">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-link">
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-current-item">Manage Item > Add New Item</li>
+                </ol>
+            </div>
+        </header>
+        <!-- /Topbar -->
 
+        <div class="greeting-field">
+            Add New Item
+        </div>
         <!-- Content -->
         <section id="content" class="table-layout animated fadeIn">
-
-
             <!-- Column Center -->
             <div class="chute chute-center">
 
                 <div class="mw1000 center-block">
 
                     <!-- Spec Form -->
-                    <div>
-                        <div class="panel">
+                    <div class="allcp-form">
+                        <div class="panel">  
+                            
                             <div class="panel-body">
-
+                            
                             <form class="form-horizontal" action="?mmopilot=edit_item&id=<?php echo $_GET['id'];?>" method="POST" enctype="multipart/form-data">
-                                
+                                        <div class="row">
+                                            <?php
+                                            $id = $_GET['id'];
+                                            $select = $db->prepare("SELECT * FROM mmo_item where idItem='$id'");
+                                            $select->execute();
+                                            $tampil = $select->fetchAll();
+                                            foreach($tampil as $value){
+                                            ?>
+                                            <input type="hidden" id="inputStandard" class="form-control" name="idItem" value="<?php echo $value['idItem']?>" disabled>
+                                            <div class="col-md-6">
+                                                <div class="section">
+                                                <h5>Nama Item</h5>
+                                                    <label class="field prepend-icon">
+                                                        <input type="text" name="itemName" id="firstname" class="gui-input"
+                                                            placeholder="Nama Item" title="masukkan nama item di sini" value="<?php echo $value['itemName']?>" required>
+                                                        <span class="field-icon">
+                                                            <i class="fa fa-slack"></i>
+                                                        </span>
+                                                    </label><br/><br/>
+                                                    
+                                                    <h5>Deskripsi Item</h5>
+                                                    <label class="field prepend-icon">
+                                                    <textarea class="gui-textarea" id="comment" name="roleDesc">
+                                                       <?php echo $value['itemDescription']?>
+                                                    </textarea>
+                                                    <span class="field-icon" title="masukkan deskripsi item di sini">
+                                                        <i class="fa fa-list"></i>
+                                                    </span>
+                                                    </label><br/><br/>
 
-                                <?php
-                                $id = $_GET['id'];
-                                $select = $db->prepare("SELECT * FROM mmo_item where idItem='$id'");
-                                $select->execute();
-                                $tampil = $select->fetchAll();
-                                foreach($tampil as $value){
-                                ?>
-                                <div class="form-group">
-                                    <label for="inputStandard" class="col-lg-3 control-label">ID Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="idItem" value="<?php echo $value['idItem']?>" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-1"></div>
+                                            <div class="col-md-5">
+                                                <div class="section">
+                                                <h5>Link Item</h5>
+                                                    <label class="field prepend-icon">
+                                                        <input type="text" name="itemLink" class="gui-input"
+                                                            placeholder="Nama Item" title="masukkan link docs/knowledge base item tersebut ()" value="<?php echo $value['itemLink']?>" required>
+                                                        <span class="field-icon">
+                                                            <i class="fa fa-link"></i>
+                                                        </span>
+                                                    </label><br/><br/>
+                                                    <h5>Item Icon</h5>
+                                                    <label class="field prepend-icon">
+                                                        <input type="file" name="itemIcon" class="gui-input"
+                                                            placeholder="Nama Item" title="Upload Icon untuk Item">
+                                                        <span class="field-icon" required>
+                                                            <i class="fa fa-upload"></i>
+                                                        </span>
+                                                    </label><br/><br/><br/><br/><br/>
+                                                    <?php }?>
+                                                    <button type="submit" class="btn btn-block btn-success" name="submit"><strong>Simpan Data</strong></button>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Nama Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="itemName" value="<?php echo $value['itemName']?>" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Deskripsi Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="itemDescription" value="<?php echo $value['itemDescription']?>" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Icon Item</label>
-                                     <div class="col-lg-8">
-                                         <div>
-                                             <input type="file" name="itemIcon" required>
-                                          </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-lg-3 control-label" for="textArea3">Link Item</label>
-                                    <div class="col-lg-8">
-                                        <div>
-                                            <input type="text" id="inputStandard" class="form-control" name="itemLink" value="<?php echo $value['itemLink']?>" required>
-                                        </div>
-                                    </div>
-                                </div>
-                                  <?php }?>
-                                <input type="submit" class="btn btn-rounded btn-primary btn-block"  name="submit" value="Simpan">
                             </form>
                         </div>
                         </div>
