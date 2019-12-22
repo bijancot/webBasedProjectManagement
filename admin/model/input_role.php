d7e4d0150f3d114b934d4f6d8941be014bb5240e
Panji Faith Baskoro <panjidia995@gmail.com>
2019-12-23 00:25:31 +0700

    merubah model input role

--- admin/model/input_role.php
+++ admin/model/input_role.php
@@ -1,14 +1,13 @@
 <?php
-// //var_dump($_POST['menuList']);
+// var_dump($_POST['menuList']);
 // $idRole = htmlentities($_POST['idRole']);
 $roleName = htmlentities($_POST['roleName']);
 $roleDesc = htmlentities($_POST['roleDesc']);
-$password = htmlentities($_POST['passwordRole']);
-$username = htmlentities($_POST['usernameRole']);
-$passhash = md5($password);
+//$password = htmlentities($_POST['passwordRole']);
+//$username = htmlentities($_POST['usernameRole']);
+//$passhash = md5($password);
 $job = htmlentities($_POST['job']);
-$idMenu = $_POST['menuList'];
-$idUser = htmlentities($_POST['idUser']);
+var_dump($idMenu = $_POST['menuList']);
 
 $query = $db->prepare("INSERT INTO `mmo_role`(`roleName`, `roleDesc`,`job`) VALUES (:roleName,:roleDesc,:job)");
 
@@ -23,19 +22,15 @@ $select->bindParam(":roleName", $roleName);
           $select->execute();
           $tampil = $select->fetchAll();
   foreach($tampil as $value){
-    $idRole = $value['idRole'];
+ $idRole = $value['idRole'];
 }
 foreach($idMenu as $ho){
+  echo $ho;
 $query1 = $db->prepare("INSERT INTO `tr_role_menu`( `idRole`, `idMenu`) VALUES (:idRole,:idMenu)");
 $query1->bindParam(":idRole", $idRole);
 $query1->bindParam(":idMenu", $ho);
 $query1->execute();
 }
-echo $idRole;
-$update = $db->prepare("UPDATE mmo_users set roleId=:idRole where idUser=:idUser");
-$update->bindParam(":idRole", $idRole);
-$update->bindParam(":idUser", $idUser);
-$update->execute();
 
 header('location:?mmopilot=managerole');
 ?>
