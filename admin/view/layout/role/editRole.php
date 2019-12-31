27edaf37f101428fff800931f6b74b8461c25029
Panji Faith Baskoro <panjidia995@gmail.com>
2019-12-31 23:47:58 +0700

    new years push : fix layout for edit role

--- admin/view/layout/role/editRole.php
+++ admin/view/layout/role/editRole.php
@@ -104,7 +104,7 @@
                                                         <i class="fa fa-list"></i>
                                                     </span>
                                                     </label><br/><br/>
-                                                    <h6>Username</h6>
+                                                    <!-- <h6>Username</h6>
                                                     <label class="field prepend-icon">
                                                         <input type="text" name="usernameRole" id="firstname" class="gui-input"
                                                             placeholder="username" title="username untuk role ini" value="<?php echo $value['username'];?>" required>
@@ -119,7 +119,7 @@
                                                         <span class="field-icon">
                                                             <i class="fa fa-key"></i>
                                                         </span>
-                                                    </label><br/><br/>
+                                                    </label><br/><br/> -->
                                                     
                                                 </div>
                                             </div>
