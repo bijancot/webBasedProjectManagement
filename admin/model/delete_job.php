df9c6802e8657a9c2d0848859750c053dd624122
lunalotussa <171111047@mhs.stiki.ac.id>
2020-02-07 19:25:39 +0700

    Add files via upload
    delete item on job

--- admin/model/delete_job.php
+++ admin/model/delete_job.php
@@ -4,6 +4,10 @@ echo $id = $_GET['id'];
 $query = $db->prepare("DELETE FROM `mmo_job` WHERE idJob= :id");
 $query->bindParam(":id", $id);
 $query->execute();
+
+$query2 = $db->prepare("DELETE FROM `tr_job_item` WHERE idJob= :id");
+$query2->bindParam(":id", $id);
+$query2->execute();
 // header('location:?mmopilot=manageitem');
 echo "<script type=\"text/javascript\">alert(' Data berhasil dihapus');document.location='?mmopilot=managejob';</script>"
 ?>
