626790369b4636b5ff37a70a78d1c41d97a0fd3c
lunalotussa <171111047@mhs.stiki.ac.id>
2020-02-10 21:58:03 +0700

    Add files via upload
    input subjob w/ new item

--- admin/model/input_subjob.php
+++ admin/model/input_subjob.php
@@ -1,35 +1,61 @@
 <?php
 //require'../conn/koneksi.php';
 if(isset($_POST['submit'])){
-
-$jobParent			= htmlentities($_POST['jobParent']);
-$idItem				= htmlentities($_POST['item']);
+echo "lala";
+echo $jobParent			= htmlentities($_POST['jobParent']);
 $idOrder			= htmlentities($_POST['order']);
-$idUser				= htmlentities($_POST['operator']);
 $jobDescription		= htmlentities($_POST['jobDescription']); 
-$time 				= htmlentities($_POST['time']);
+$timee 				= htmlentities($_POST['time']);
 $itemTarget			= htmlentities($_POST['itemTarget']);
-$priority			= htmlentities($_POST['priority']);
+var_dump($idItem = $_POST['idItem']);
 //echo $parent 			= htmlentities($_POST['parent']);
 $status = "1";
-$cr = "admin";
-$query = $db->prepare("INSERT INTO `mmo_job`
-(`idOperator`, `idOrder`, `jobParent` ,`idItem`, `jobDescription`, `jobStatus`, `time`, `itemTarget`, `priority`, `createdBy`) 
-VALUES (:idOperator,:idOrder,:jobParent,:idItem,:jobDesc,:jobStatus,:time,:itemTarget,:priority,:createdBy)");
+$cr 	= "admin";
+$idPriority	= "1";
+$query = $db->prepare("INSERT INTO `mmo_job`(`idOrder`,`jobParent`, `jobDescription`,`time`, `jobStatus`, `itemTarget`, `idPriority`, `createdBy`) VALUES (:idOrder,:jobParent,:jobDesc,:timee,:jobStatus,:itemTarget,:idPriority,:createdBy)");
 
-$query->bindParam(":idOperator",$idUser);
-$query->bindParam(":idOrder",$idOrder);
+
 $query->bindParam(":jobParent",$jobParent);
-$query->bindParam(":idItem",$idItem);
+$query->bindParam(":idOrder",$idOrder);
 $query->bindParam(":jobDesc",$jobDescription);
 $query->bindParam(":jobStatus",$status);
-$query->bindParam(":time",$time);
+$query->bindParam(":timee",$timee);
 $query->bindParam(":itemTarget",$itemTarget);
-$query->bindParam(":priority",$priority);
+$query->bindParam(":idPriority",$idPriority);
 $query->bindParam(":createdBy",$cr);
 
+$query->execute();
 
- $query->execute();
- header('location:?mmopilot=managejob');
+$select = $db->prepare("SELECT idJob FROM mmo_job where jobDescription=:jobDescription");
+$select->bindParam(":jobDescription", $jobDescription);
+          $select->execute();
+          $tampil = $select->fetchAll();
+  foreach($tampil as $value){
+ $idJob = $value['idJob'];
+}
+foreach($idItem as $it){
+  echo $it;
+$query1 = $db->prepare("INSERT INTO `tr_job_item`( `idJob`, `idItem`,`createdDate`,`lastUpdate`) VALUES (:idJob,:idItem,NOW(),NOW())");
+$query1->bindParam(":idJob", $idJob);
+$query1->bindParam(":idItem", $it);
+$query1->execute();
+}
+
+ // $select = $db->prepare("SELECT idJob FROM mmo_job where idOrder=$idOrder");
+ // $select->execute();
+ // $tampil = $select->fetchAll();
+ 
+ // // foreach ($tampil as $key) {
+ //     echo $idid = $key['idJob'];
+ // }
+ // $quer2 = $db->prepare("UPDATE mmo_operator_shift set idJob=:idJob where idOrder=:idOrder");
+
+ // $quer2->bindParam(":idJob", $idid);
+ // $quer2->bindParam(":idOrder", $idOrder);
+ // $quer2->execute();
+ // NB : SORRY TAK KOMEN TAPI TAK CEK GAK JALAN
+ echo "<script type=\"text/javascript\">alert(' Data berhasil disimpan');document.location='?mmopilot=managejob';</script>";
+}else{
+echo "<script type=\"text/javascript\">alert('eror');document.location='?mmopilot=managejob';</script>";
 }
 ?>
