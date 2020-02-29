04701890c801e1519de6ab3ecdf217586ee9cd89
lunalotussa <171111047@mhs.stiki.ac.id>
2020-02-29 17:37:03 +0700

    Add files via upload
    input progress report fix

---
+++ admin/model/input_progressreport.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST['submit'])){
+echo $idJob				= htmlentities($_POST['idJob']);
+echo $idOperator		= htmlentities($_POST['idOperator']);
+echo $progressDate		= htmlentities($_POST['progressDate']);
+echo $itemAchived		= htmlentities($_POST['itemAchived']);
+echo $progressNote		= htmlentities($_POST['progressNote']);
+
+$query = $db->prepare("INSERT INTO `mmo_job_progress`(`idJob`, `idOperator`, `itemArchived`, `progressDate`, `progressDesc`) VALUES(:idJob,:idOperator,:itemAchived,:progressDate,:progressNote)");
+
+$query->bindParam(":idJob",$idJob);
+$query->bindParam(":idOperator",$idOperator);
+$query->bindParam(":progressDate",$progressDate);
+$query->bindParam(":itemAchived",$itemAchived);
+$query->bindParam(":progressNote",$progressNote);
+
+$query->execute();
+
+header('location:?mmopilot=progressreport');
+}
+?>
