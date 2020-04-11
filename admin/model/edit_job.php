9c9cde56c88929ea4bb77d83c4da2b4d640de356
lunalotussa <171111047@mhs.stiki.ac.id>
2020-04-11 23:05:04 +0700

    Add files via upload
    edit job aksi (looping cb error)

--- admin/model/edit_job.php
+++ admin/model/edit_job.php
@@ -2,35 +2,41 @@
 //require'../conn/koneksi.php';
 if(isset($_POST['submit'])){
 
-$idJob 				= htmlentities($_GET['id']);
-$idItem				= htmlentities($_POST['item']);
-$idOrder			= htmlentities($_POST['order']);
-$idUser				= htmlentities($_POST['operator']);
-$jobDescription		= htmlentities($_POST['jobDescription']); 
-$time 				= htmlentities($_POST['time']);
-$itemTarget			= htmlentities($_POST['itemTarget']);
-$itemByTime			= $itemTarget/$time;
-$priority			= htmlentities($_POST['priority']);
-
+echo $idJob 				= htmlentities($_GET['id']); echo"<br>";
+echo $idOrder				= htmlentities($_POST['order']); echo"<br>";
+echo $jobDescription		= htmlentities($_POST['jobDescription']); echo"<br>";
+echo $timee 				= htmlentities($_POST['time']); echo"<br>";
+echo $itemTarget			= htmlentities($_POST['itemTarget']); echo"<br>";
+echo $idPriority			= htmlentities($_POST['idPriority']);echo"<br>";
+$status 					= "1";
+$cr 						= "admin";
+var_dump($idItem = $_POST['idItem']);
 
 $query = $db->prepare("UPDATE `mmo_job` SET 
-						`idOperator`='$idUser',
-						`idOrder`='$idOrder',
-						`jobParent`=null,
-						`idItem`='$idItem',
-						`jobDescription`='$jobDescription',
-						`jobStatus`='0',
-						`time`='$time',
-						`itemTarget`='$itemTarget',
-						`itemByTime`='$itemByTime',
-						`priority`='$priority',
-						`createdBy`='admin',
-						`updateBy`='',
-						`updatedDate`=NOW(),
-						`status`=''
-						WHERE `idJob`='$idJob'");
+					`idOrder`='$idOrder',
+					`idPriority`='$idPriority',
+					`jobDescription`='$jobDescription',
+					`jobStatus`='$status',
+					`time`='$timee',
+					`itemTarget`='$itemTarget',
+					`createdBy`='$cr',
+					`updatedDate`=NOW() 
+					WHERE `idJob`='$idJob'");
 
- $query->execute();
+$query->execute();
+	
+$delete = $db->prepare("DELETE FROM tr_job_item where idJob=$idJob");
+
+$delete->execute();
+
+foreach($idItem as $it){
+  echo $it;
+$query1 = $db->prepare("INSERT INTO `tr_job_item`( `idJob`, `idItem`,`createdDate`,`lastUpdate`) VALUES (:idJob,:idItem,NOW(),NOW())");
+$query1->bindParam(":idJob", $idJob);
+$query1->bindParam(":idItem", $it);
+$query1->execute();
+}
+
  header('location:?mmopilot=managejob');
 
 }
